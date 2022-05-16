bd8e182974e771de9507ac530d31f802d113e8ce
Teslagirl69 <teslagirl17@gmail.com>
2022-05-16 14:28:29 +0300

    Init barbers table in confugure

--- app.rb
+++ app.rb
@@ -5,17 +5,34 @@ require 'sqlite3'
 
 
 
-	def get_db
-		@db =  SQLite3::Database.new 'barbershop.db'
-	  @db.results_as_hash = true
-  return @db
+#создаем метод, в котором отпределяем по имени есть у нас такой парикмахер или нет
+def is_barber_exists? db, name
+db.execute('select * from Barbers where name=?', [name]).length > 0
+end
+
+#доп метод для проверки !is_barber_exists?, проходясь по всему массиву. если барбер не существует, то запрос по втсавкебарбера в бд
+
+def seed_db db, barbers
+	barbers.each do |barber|
+		if !is_barber_exists? db, barber
+			db.execute 'insert into Barbers (name) values (?)', [barber]
+		end
 	end
+end
 
 
+
+def get_db
+		db =  SQLite3::Database.new 'barbershop.db'
+	  db.results_as_hash = true
+  return db
+end
+
+#запускается configure каждый раз при запуске приложения, создает таблицы, если нужно
 configure do
-	@db = get_db
+	db = get_db
 
-	@db.execute 'CREATE TABLE IF NOT EXISTS "Users" (
+	db.execute 'CREATE TABLE IF NOT EXISTS "Users" (
 	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
 	"user_name"	TEXT,
 	"phone"	TEXT,
@@ -23,10 +40,16 @@ configure do
 	"time"	TEXT,
 	"master"	TEXT,
 	"colour"	TEXT
-);
 	)'
-end
 
+	db.execute 'CREATE TABLE IF NOT EXISTS "Barbers"
+	(
+	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"name"	TEXT
+	)'
+
+seed_db db, ['Мария', 'Екатерина', 'Игнат', 'Анатолий']
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
@@ -57,31 +80,20 @@ post '/visit' do
 
 hh = {:user_name => "Что-то пошло не так, введите имя", :phone => "Что-то пошло не так, введите телефон", :date => "Что-то пошло не так, выберите дату ", :time => "Что-то пошло не так, выберите время "}
 
-hh.each do |key, value|
+	hh.each do |key, value|
 
-	if params[key] == ""
-		@error = hh[key]
-		return erb :visit
-
+		if params[key] == ""
+			@error = hh[key]
+			return erb :visit
+		end
 	end
-end
 
-
-
-@db = get_db
-@db.execute 'insert into Users (user_name, phone, date, time, master, colour) values (?,?,?,?,?,?)', [@user_name, @phone, @date, @time, @master, @ok_colour]
+db = get_db
+db.execute 'insert into Users (user_name, phone, date, time, master, colour) values (?,?,?,?,?,?)', [@user_name, @phone, @date, @time, @master, @ok_colour]
 
 #сообщение после ввода данных пользователя
 @title = 'Спасибо, запись прошла успешно'
 @message = "#{@user_name}, мы будем ждать Вас #{@date}, время записи: #{@time}. Ваш мастер #{@master}. Вы выбрали #{@ok_colour} цвет для окрашивания"
-
-=begin
-запись данных, предоставленных пользователем в отдельный файл
-		f = File.open("users_barbers.txt", "a")
-		f.write "User: #{@user_name}, Phone: #{@phone}, Date: #{@date}, Master #{@master}, color: #{@ok_colour}\n\r"
-		f.close
-=end
-
 
 erb :message
 
@@ -144,11 +156,12 @@ end
 
 get '/show_users' do
 #сделали запрос
-	@db = get_db
+	db = get_db
 
 #получили результаты, в 	@results  получили набор данных
 
-@results = @db.execute 'select * from Users order by id desc'
-@db.close
+@results = db.execute 'select * from Users order by id desc'
+db.close
 erb :show_users
 end
+
